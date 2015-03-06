925921d79cbbdbed6313d7af5e7b5e9551c014e8
kkanzelmeyer <kanzelm4@gmail.com>
2015-03-05 19:45:23 -0600

    updated README

--- src/tests/HelixTest.java
+++ src/tests/HelixTest.java
@@ -14,14 +14,14 @@ import helix.enums.WaypointType;
 public class HelixTest {
 
     public static void main(String[] args) {
-        Location roi = Location.HSV_DALLAS_WATER_TOWER; // Establish the center location of the helix
+        Location roi = Location.HSV_LOWE_MILL_WATER_TOWER; // Establish the center location of the helix
         Helix helix = new Helix(roi.location(), 1, 10, 50, 15, 180, false); // Build the helix with custom params
-        helix.waypointType(WaypointType.SPLINE); // Set the waypoint type
+        helix.waypointType(WaypointType.WAYPOINT); // Set the waypoint type
         helix.circleAtTop(true); // make the drone do a planar circle at the top of the helix
         helix.descend(true); // make the drone do a descending helix
         helix.setDirection(Direction.CLOCKWISE); // set the direction to clockwise or counter-clockwise
         helix.generateHelicalWaypoints(); // must call  this method to generate the waypoints
-        // helix.printKML();
-        helix.printMissionCommands(); // use to generate the APM Planner style text output
+        helix.printKML();
+//        helix.printMissionCommands(); // use to generate the APM Planner style text output
     }
 }
